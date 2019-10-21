88cac9e7efa75fae0e738c72695084040de0efae
Zheka6285 <zhekadee689334@gmail.com>
2019-10-21 11:35:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	
+	setlocale(LC_ALL, "rus");
+
+	int a;
+	cout << "Квадрат: 1\nТриугольник: 2\nКрест: 3\n";
+	cout << "input a: ";
+	cin >> a;
+	cout << "\n";
+	switch (a) {
+
+	case 1: cout << "**********\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "*        *\n";
+		cout << "**********\n" << "\n"; break;
+
+	case 2: cout << "*\n";
+		cout << "**\n";
+		cout << "* *\n";
+		cout << "*  *\n";
+		cout << "*   *\n";
+		cout << "*    *\n";
+		cout << "*     *\n";
+		cout << "*      *\n";
+		cout << "*       *\n";
+		cout << "**********\n" << "\n"; break;
+
+	case 3: cout << "    *    \n";
+		cout << "    *    \n";
+		cout << "    *    \n";
+		cout << "    *    \n";
+		cout << "*********\n";
+		cout << "    *    \n";
+		cout << "    *    \n";
+		cout << "    *    \n";
+		cout << "    *    \n" << "\n"; break;
+	default: cout << "Ошибка" << "\n";
+	}
+
+
+}
+
